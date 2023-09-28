336212fc5f4c1c2caef8ff229e80165d3b24146e
Ketti <esgoncharova0@gmail.com>
2023-09-28 22:20:23 +0300

    delete table

--- MigrationScripts/AddForeignKeysMigration.cs
+++ MigrationScripts/AddForeignKeysMigration.cs
@@ -497,9 +497,9 @@ namespace MigrationScripts
                   .FromTable("tblMessage").ForeignColumn("MessageUpdatedBy")
                   .ToTable("tblCustomer").PrimaryColumn("CustomerId");
 
-            Create.ForeignKey("FK_tblMessageGroup_tblEvent_Event")
-                  .FromTable("tblMessageGroup").ForeignColumn("EventId")
-                  .ToTable("tbEvent").PrimaryColumn("EventId");
+            //Create.ForeignKey("FK_tblMessageGroup_tblEvent_Event")
+            //      .FromTable("tblMessageGroup").ForeignColumn("EventId")
+            //      .ToTable("tbEvent").PrimaryColumn("EventId");
             Create.ForeignKey("FK_tblMessageGroup_tblCustomer_Customer")
                   .FromTable("tblMessageGroup").ForeignColumn("CustomerId")
                   .ToTable("tblCustomer").PrimaryColumn("CustomerId");
@@ -1236,8 +1236,8 @@ namespace MigrationScripts
             Delete.ForeignKey("FK_tblMessage_tblCustomer_UpdatedBy")
                   .OnTable("tblMessage");
 
-            Delete.ForeignKey("FK_tblMessageGroup_tblEvent_Event")
-                  .OnTable("tblMessageGroup");
+            //Delete.ForeignKey("FK_tblMessageGroup_tblEvent_Event")
+            //      .OnTable("tblMessageGroup");
             Delete.ForeignKey("FK_tblMessageGroup_tblCustomer_Customer")
                   .OnTable("tblMessageGroup");
             Delete.ForeignKey("FK_tblMessageGroup_tblCustomer_CreatedBy")
