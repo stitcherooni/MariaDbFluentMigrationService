53a37487ff2a4f2ad354a46908f39822d9ce15b8
Ketti <esgoncharova0@gmail.com>
2023-09-29 21:57:39 +0300

    1

--- MigrationScripts/InsertTestData.cs
+++ MigrationScripts/InsertTestData.cs
@@ -1,87 +1,214 @@
-﻿using FluentMigrator;
+﻿//using FluentMigrator;
 
-namespace MigrationScripts
-{
-    [Migration(2)]
-    public class InsertTestData : Migration
-    {
-        public override void Up()
-        {
-            Insert.IntoTable("tblCustomer")
-                .Row(new
-                {
-                    CustomerId = 1,
-                    CustomerFirstName = "CustomerFirstName",
-                    CustomerLastName = "CustomerLastName",
-                    CustomerEmail = "CustomerEmail@email.com",
-                    CustomerPassword = "CustomerPassword",
-                    CustomerCreatedBy = 1,
-                    ApplicationID = 1
-                });
-            Insert.IntoTable("tblCustomer")
-                .Row(new
-                {
-                    CustomerId = 0,
-                    CustomerFirstName = "CustomerFirstName",
-                    CustomerLastName = "CustomerLastName",
-                    CustomerEmail = "CustomerEmail@email.com",
-                    CustomerPassword = "CustomerPassword",
-                    CustomerCreatedBy = 1,
-                    ApplicationID = 170
-                });
+//namespace MigrationScripts
+//{
+//    [Migration(2)]
+//    public class InsertTestData : Migration
+//    {
+//        public override void Up()
+//        {
+//            Insert.IntoTable("tblCustomer")
+//                .Row(new
+//                {
+//                    CustomerId = 1,
+//                    CustomerFirstName = "CustomerFirstName",
+//                    CustomerLastName = "CustomerLastName",
+//                    CustomerEmail = "CustomerEmail@email.com",
+//                    CustomerPassword = "CustomerPassword",
+//                    CustomerCreatedBy = 1,
+//                    ApplicationID = 1
+//                });
+//            Insert.IntoTable("tblCustomer")
+//                .Row(new
+//                {
+//                    CustomerId = 0,
+//                    CustomerFirstName = "CustomerFirstName",
+//                    CustomerLastName = "CustomerLastName",
+//                    CustomerEmail = "CustomerEmail@email.com",
+//                    CustomerPassword = "CustomerPassword",
+//                    CustomerCreatedBy = 1,
+//                    ApplicationID = 170
+//                });
 
-            Insert.IntoTable("tblSchool")
-               .Row(new
-               {
-                   SchoolId = 0,
-                   SchoolPTAName = "SchoolPTAName",
-                   SchoolCreatedBy = 0
-               });
-            Insert.IntoTable("tblSchool")
-                .Row(new
-                {
-                    SchoolId = 1,
-                    SchoolPTAName = "SchoolPTAName",
-                    SchoolCreatedBy = 1
-                });
+//            Insert.IntoTable("tblSchool")
+//               .Row(new
+//               {
+//                   SchoolId = 0,
+//                   SchoolPTAName = "SchoolPTAName",
+//                   SchoolCreatedBy = 0
+//               });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 170,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1881,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 144,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 329,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 329,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1959,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1827,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//               .Row(new
+//               {
+//                   SchoolId = 466,
+//                   SchoolPTAName = "SchoolPTAName",
+//                   SchoolCreatedBy = 0
+//               });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 806,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 693,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 871,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1143,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 884,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 889,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 296,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1052,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
 
-            Insert.IntoTable("tblSchool")
-                .Row(new
-                {
-                    SchoolId = 170,
-                    SchoolPTAName = "SchoolPTAName",
-                    SchoolCreatedBy = 0
-                });
-        }
+//            Insert.IntoTable("tblSchool")
+//   .Row(new
+//   {
+//       SchoolId = 567,
+//       SchoolPTAName = "SchoolPTAName",
+//       SchoolCreatedBy = 0
+//   });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 2,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 1
+//                });
 
-        public override void Down()
-        {
-            Delete.FromTable("tblCustomer")
-                .Row(new
-                {
-                    CustomerID = 1
-                });
-            Delete.FromTable("tblCustomer")
-                .Row(new
-                {
-                    CustomerID = 0
-                });
-            Delete.FromTable("tblSchool")
-                .Row(new
-                {
-                    SchoolId = 0
-                });
-            Delete.FromTable("tblSchool")
-                .Row(new
-                {
-                    SchoolId = 1
-                });
+//            Insert.IntoTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 466,
+//                    SchoolPTAName = "SchoolPTAName",
+//                    SchoolCreatedBy = 0
+//                });
+//        }
 
-            Delete.FromTable("tblSchool")
-                .Row(new
-                {
-                    SchoolId = 170
-                });
-        }
-    }
-}
+//        public override void Down()
+//        {
+//            Delete.FromTable("tblCustomer")
+//                .Row(new
+//                {
+//                    CustomerID = 1
+//                });
+//            Delete.FromTable("tblCustomer")
+//                .Row(new
+//                {
+//                    CustomerID = 0
+//                });
+//            Delete.FromTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 0
+//                });
+//            Delete.FromTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 1
+//                });
+
+//            Delete.FromTable("tblSchool")
+//                .Row(new
+//                {
+//                    SchoolId = 170
+//                });
+//        }
+//    }
+//}
